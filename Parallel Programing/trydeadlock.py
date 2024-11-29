1b27182659a0b2252ea6fa4ac55d7a8d87ba7935
Runyuk <157195336+Runyuk@users.noreply.github.com>
2024-11-29 11:43:29 +0700

    def change

--- Parallel Programing/trydeadlock.py
+++ Parallel Programing/trydeadlock.py
@@ -5,17 +5,19 @@ lock1 = threading.Lock()
 lock2 = threading.Lock()
 
 def thread1():
-    with lock1:
-        print('thread 1 : mengunci lock1')
-        threading.Event().wait(1)
-    with lock2:
-        print('thread 1: mengunci lock2')
+    if lock1.acquire(timeout=1):
+        print('thread 1: Lock 1 acuired, waiting for Lock 2...')
+        if lock2.acquire(timeout=1):
+            print('Trhead 1: Lock 2 Acuired')
+            lock2.release()
+        lock1.release()
 def thread2():
-    with lock2:
-        print('thread 2 : mengunci lock2')
-        threading.Event().wait(1)
-    with lock2:
-        print('thread 2: mengunci lock1')
+    if lock2.acquire(timeout=1):
+        print('thread 2: Lock 2 acuired, waiting for Lock 1...')
+        if lock1.acquire(timeout=1):
+            print('Trhead 2: Lock 1 Acuired')
+            lock1.release()
+        lock2.release()
 
 t1 = threading.Thread(target=thread1)
 t2 = threading.Thread(target=thread2)
